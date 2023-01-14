9e1db8492329d4a0ff0aafdea000045368314e9c
TÙNG PHAN NGUYỄN THANH <19120424@student.hcmus.edu.vn>
2023-01-14 11:35:33 +0700

    Use docker instead of docker compose in main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,7 @@
-docker compose up -d --wait
+docker build -t retrieval-api ./retrieval_api
+docker build -t tryon-api ./tryon_api   
+
+docker run -d -p 8081:8081 --name retrieval-api retrieval-api
+docker run -d -p 8082:8082 --name tryon-api tryon-api
 
 python main_entry/src/main.py
